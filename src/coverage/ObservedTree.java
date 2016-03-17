6393d22fb4b1062763a820c4b1e91f5b9dd92123
meng2010 <y.meng201011@gmail.com>
2016-03-17 14:06:43 -0400

    omcdc

--- src/coverage/ObservedTree.java
+++ src/coverage/ObservedTree.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class ObservedTree {
  
-    private TreeNode root;
+    private ObservedTreeNode root;
     
-    public ObservedTree(TreeNode root) {
+    public ObservedTree(ObservedTreeNode root) {
     	setroot(root);
     }
  
-    public TreeNode getroot() {
+    public ObservedTreeNode getroot() {
         return this.root;
     }
  
-    public void setroot(TreeNode root) {
+    public void setroot(ObservedTreeNode root) {
     	if (root == null) {
     		throw new IllegalArgumentException("null root!");
     	}
@@ -25,27 +25,50 @@ public class ObservedTree {
     /*
      * convert the tree to a list
      */
-    public List<TreeNode> toList() {
-        List<TreeNode> list = new ArrayList<TreeNode>();
+    public List<ObservedTreeNode> toList() {
+        List<ObservedTreeNode> list = new ArrayList<ObservedTreeNode>();
         traverse(root, list);
         return list;
+    }
+    
+    /*
+     * convert a subtree rooted with given sub-root to a list
+     */
+    public List<ObservedTreeNode> subtreeToList(ObservedTreeNode subroot) {
+    	List<ObservedTreeNode> list = new ArrayList<ObservedTreeNode>();
+    	traverse(subroot, list);
+    	return list;
     }
     
     @Override
     public String toString() {
         return toList().toString();
+//    	List<String> list = new ArrayList<String>();
+//    	print(this.root, list);
+//    	return list.toString();
     }
     
     /*
      * Traverse the sub-tree of given node
      */
-    private void traverse(TreeNode node, List<TreeNode> list) {
-        list.add(node);
-        for (TreeNode data : node.getChildren()) {
-            traverse(data, list);
+    private void traverse(ObservedTreeNode parent, List<ObservedTreeNode> list) {
+        list.add(parent);
+        for (ObservedTreeNode child : parent.getChildren()) {
+            traverse(child, list);
         }
     }
     
-    //TODO:traverse the sub-tree BFS
-    
+    /*
+     * print subtree (DFS) given root
+     */
+    public void print(ObservedTreeNode node) {
+    	if (node == null) {
+    		return;
+    	}
+    	System.out.println("Children of [" + node.getData() + "]: " + node.getChildren().toString());
+    	for (ObservedTreeNode child : node.getChildren()) {
+    		print(child);
+    	}
+    }
+        
 }
--- src/coverage/ObservedTreeNode.java
+++ src/coverage/ObservedTreeNode.java
@@ -8,12 +8,7 @@ public class ObservedTreeNode {
     public ObservedTreeNode parent;
     public List<ObservedTreeNode> children;
  
-    public ObservedTreeNode() {
-    	super();
-    }
-
     public ObservedTreeNode(String data) {
-        this();
         setData(data);
     }
 
@@ -40,7 +35,7 @@ public class ObservedTreeNode {
             children = new ArrayList<ObservedTreeNode>();
         }
         child.parent = this;
-        children.add(child);
+        this.children.add(child);
     }
  
     public String getData() {
@@ -51,23 +46,19 @@ public class ObservedTreeNode {
         this.data = data;
     }
     
-    
-    //TODO: represent the node in a new way
-//    @Override
-//    public String toString() {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(getData()).append(" -> [");
-//        int i = 0;
-//        for (TreeNode e : getChildren()) {
-//            if (i > 0) {
-//                sb.append(" , ");
-//            }
-//            sb.append(e.getData());
-//            i++;
-//        }
-//        sb.append("] ");
-//        return sb.toString();
-//    }
+    // return all leaf nodes of specific node
+    public List<String> getAllLeaves() {
+    	List<String> leaves = new ArrayList<String>();
+    	if (this.children == null) {
+            leaves.add(this.data);
+        } else {
+            for (ObservedTreeNode child : this.children) {
+                leaves.addAll(child.getAllLeaves());
+            }
+        }
+    	
+    	return leaves;
+    }
     
     @Override
     public String toString() {
